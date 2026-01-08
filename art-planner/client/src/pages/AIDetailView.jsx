cf0c35fb8916a9aaa3e7d8e0f64927be41cb48ca
Anna <anuta2106200506@gmail.com>
2026-01-08 23:17:58 +0100

    update project

--- art-planner/client/src/pages/AIDetailView.jsx
+++ art-planner/client/src/pages/AIDetailView.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { ChevronLeft, ChevronRight, Send } from 'lucide-react';
 import API from '../api/axiosConfig';
 
