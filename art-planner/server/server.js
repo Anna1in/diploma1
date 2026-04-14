c37644e7c491ef62db8d01a396563bb79c94db91
Anna <anuta2106200506@gmail.com>
2026-04-14 19:17:47 +0200

    add Base64 4 photos -2

--- art-planner/server/server.js
+++ art-planner/server/server.js
@@ -11,7 +11,8 @@ const multer = require('multer');
 const { User, Art } = require('./models/Schemas');
 
 const app = express();
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use(cors());
 
 // Налаштування Multer для завантаження малюнків
