18addc860af195c8c455e038d52ec93e4a05febc
Anna <anuta2106200506@gmail.com>
2026-06-01 00:33:41 +0200

    edit project-3

--- art-planner/server/utils/ai.js
+++ art-planner/server/utils/ai.js
@@ -3,7 +3,7 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 async function analyzeArtWithGemini(base64Image, userPrompt) {
-    // Використовуємо 1.5-flash, бо вона стабільніша з квотами
+
     const model = genAI.getGenerativeModel({
         model: "gemini-flash-latest"
     });
